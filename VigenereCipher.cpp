65ab03e71ccbce2e7e9776678d6a9f85924db14b
Kashaf Zia <kashafzia100@gmail.com>
2024-06-27 01:38:18 +0500

    VigenereCipher modified with exception handling

--- VigenereCipher.cpp
+++ VigenereCipher.cpp
@@ -1,39 +1,55 @@
 #include "VigenereCipher.h"
+#include <stdexcept>
 
-VigenereCipher::VigenereCipher(const string& p,const string& c,const string& k):Cryptography(p,c), key(k) {}
+VigenereCipher::VigenereCipher(const string& p, const string& c, const string& k)
+    : Cryptography(p, c), key(k) {}
 
 void VigenereCipher::set_key(const string& k) {
     key = k;
 }
 
 string VigenereCipher::encrypt() {
-    string encrypted_text;
-    for (size_t i = 0; i < plaintext.length(); ++i) {
-        char plain_char = plaintext[i];
-        char key_char = key[i % key.length()];
-        char encrypted_char = 'A' + (plain_char - 'A' + key_char - 'A') % 26;
-        encrypted_text += encrypted_char;
+    try {
+        string encrypted_text;
+        if (plaintext.empty() || key.empty()) {
+            throw std::invalid_argument("Empty plaintext or key.");
+        }
+        for (size_t i = 0; i < plaintext.length(); ++i) {
+            char plain_char = plaintext[i];
+            char key_char = key[i % key.length()];
+            char encrypted_char = 'A' + (plain_char - 'A' + key_char - 'A') % 26;
+            encrypted_text += encrypted_char;
+        }
+        ciphertext = encrypted_text;
+        return ciphertext;
+    } catch (const std::exception& e) {
+        throw e; // Rethrow the exception for handling in the caller code
     }
-    ciphertext = encrypted_text;
-    return ciphertext;
 }
 
 string VigenereCipher::decrypt() {
-    string decrypted_text;
-    for (size_t i = 0; i < ciphertext.length(); ++i) {
-        char cipher_char = ciphertext[i];
-        char key_char = key[i % key.length()];
-        char decrypted_char = 'A' + (cipher_char - key_char + 26) % 26;
-        decrypted_text += decrypted_char;
+    try {
+        string decrypted_text;
+        if (ciphertext.empty() || key.empty()) {
+            throw std::invalid_argument("Empty ciphertext or key.");
+        }
+        for (size_t i = 0; i < ciphertext.length(); ++i) {
+            char cipher_char = ciphertext[i];
+            char key_char = key[i % key.length()];
+            char decrypted_char = 'A' + (cipher_char - key_char + 26) % 26;
+            decrypted_text += decrypted_char;
+        }
+        plaintext = decrypted_text;
+        return plaintext;
+    } catch (const std::exception& e) {
+        throw e; // Rethrow the exception for handling in the caller code
     }
-    plaintext = decrypted_text;
-    return plaintext;
 }
-void VigenereCipher::set_plaintext(const string& p)
-{
-    plaintext=p;
+
+void VigenereCipher::set_plaintext(const string& p) {
+    plaintext = p;
 }
-void VigenereCipher::set_ciphertext(const string& c)
-{
-    ciphertext=c;
+
+void VigenereCipher::set_ciphertext(const string& c) {
+    ciphertext = c;
 }
